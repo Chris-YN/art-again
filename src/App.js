da80811e79fad0202240feb79387f043ca4495ef
Chris <christopher.yun.3@gmail.com>
2022-01-17 12:06:28 -0500

    commit-01-main initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+
+
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const apiKey = "rQRAxqMb";
+
+  const [art, setArt] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Make an API call to the Rijks Museum API
+  useEffect(()=>{
+    axios({
+      url: "https://www.rijksmuseum.nl/api/en/collection",
+      method: "GET",
+      dataRespsonse: "json",
+      params: {
+        key: apiKey,
+        imgonly: true,
+        q: searchTerm,
+      }
+    }).then((response)=>{
+      console.log(response.data.artObjects);
+      setArt(response.data.artObjects);
+    });
+  }, [searchTerm]);
+
+  const handleInput = function(event){
+    console.log("does this work?", event.target.value);
+    setUserInput(event.target.value); 
+  }
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    setSearchTerm(userInput);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Welcome back to the Art Museum</h1>
+
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="search">Search for art: </label>
+        <input type="text" id="search" onChange={ handleInput } value={userInput}/>
+        <button>Search</button>
+      </form>
+
+      {art.map((artwork)=>{
+        return (
+          <div key={artwork.id}>
+            <h2>{artwork.longTitle}</h2>
+            <img src={artwork.webImage.url} alt={artwork.title} />
+          </div>
+        )
+      })}
     </div>
   );
 }
 
 export default App;
+
+
+//key rQRAxqMb
